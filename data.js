89d84e81d7bdc2caca4acd48423fdce7c9234ac0
ValleLuca <valle.lucaAb@outlook.it>
2022-01-13 15:00:04 +0100

    Funge tutto teoricamente

--- data.js
+++ data.js
@@ -1,7 +1,12 @@
 var app = new Vue({
     el: '#container',
     data: {
-      messaggio: 'Sveglia le 3060 costano troppo',
-      immagine:'https://cdn.videocardz.com/1/2020/12/NVIDIA-RTX-3060-Ti-Hero.jpg'
+      messaggio: 'Sveglia le RTX costano troppo',
+      immagine:'https://media0.giphy.com/media/sDcfxFDozb3bO/giphy.gif?cid=ecf05e47nqwlad28il572djz2t232b40pkgc5uuzs8u2qlaq&rid=giphy.gif&ct=g',
+    },
+    methods: {
+        compra: function() {
+            this.immagine = 'https://media0.giphy.com/media/3o6Zt3FIwTxfo7qhfW/giphy.gif?cid=ecf05e4758hwve1x4639wa08qj94u6waqmza9spesxduyzj3&rid=giphy.gif&ct=g';
+        }
     }
   })
