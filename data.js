8a53e17cebd90ffa6f822c94edd432642becfa9e
ValleLuca <valle.lucaAb@outlook.it>
2022-01-13 13:11:42 +0100

    inizio

---
+++ data.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#container',
+    data: {
+      messaggio: 'Sveglia le 3060 costano troppo',
+      immagine:'https://cdn.videocardz.com/1/2020/12/NVIDIA-RTX-3060-Ti-Hero.jpg'
+    }
+  })
